c4905263b9443e49974067bf3b1edfdf046b8a11
Milidrag <m-ivkic@gmx.at>
2022-11-21 08:19:36 +0100

    Enumeration of months in Python

---
+++ Enumeration/T2E1/main.py
@@ -0,0 +1,4 @@
+months = ["JAENNER", "FEBRUAR", "MAERZ", "APRIL", "MAI", "JUNI", "JULI", "AUGUST", "SEPTEMBER", "OKTOBER", "NOVEMBER", "DEZEMBER"]
+
+for num, month in enumerate(months, 1):
+    print(num, month)
